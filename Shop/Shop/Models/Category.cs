bdd5e3b4205cb7e7a64b508f2db2299e97015613
Pc-Komputer\Pc <pyszczakjakub@gmail.com>
2018-05-06 13:43:01 +0200

    Added new classes of model

---
+++ Shop/Shop/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Shop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string NameOfCategory { get; set; }
+        public string DescriptionOfCategory { get; set; }
+        public string NameOfFileIcon { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ Shop/Shop/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public DateTime AddedDateTime { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public decimal ValueOfOrder { get; set; }
+
+        List<PositionsOrder> PositionsOrder { get; set; }
+    }
+}
